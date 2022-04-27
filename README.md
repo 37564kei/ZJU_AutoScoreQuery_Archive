8e63ee9fad2d178e35abc1ee2c5244c3893eee1d
XuanInsr <2087868649@qq.com>
2022-04-27 12:22:35 +0800

    第二版代码！

---
+++ README.md
@@ -0,0 +1,122 @@
+# ZJUAutoScoreQuery
+
+浙江大学本科教务网自动查分脚本	 _by XuanInsr_
+
+
+
+如果图片加载不出来，请看 [自动查分脚本QWQ · 语雀 (yuque.com)](https://www.yuque.com/xianyuxuan/coding/auto_score_query)！
+
+
+
+参考了 [ZJU-Clock-In 自动打卡脚本](https://github.com/furry-potato-maker/ZJU-Clock-In)，部分代码直接取自其中。参考的其他代码附后。
+
+
+
+## 一些说明
+
+本脚本仅为个人学习 GitHub Action 和爬虫的试验作品，不适合传播；本脚本设计的查询频率是 10 分钟每次，并不会对服务器造成明显压力。
+
+由于我此前并没有学过 CI 和 Python 和爬虫，因此很多地方写的比较愚蠢或包含错误；敬请批评指正😝。
+
+由于我不会！所以有的时候运行的很慢或者失败，我并不能解释原因QAQ 如果有修改意见敬请赐教！
+
+
+
+### 关于数据隐私
+
+本脚本代码完全公开，使用到的教务网账号、密码、GitHub Token 以及收件邮箱均在 GitHub Actions Secrets 中加密保存，并不会通过 fork 传递，也不会被作者或者其他人得知。
+
+为了对比分数更新，代码必然需求以某种方式保存上次查询的结果；我们将其直接保存在仓库中，但是保存的仅有选课课号的哈希值列表，并不包含绩点等信息，因此基本可以认为因为此方式除了泄露了选课数目（和可能的少数选课内容）以外没有泄露其他更多信息。
+
+代码运行的过程中会访问到用户的所有选课内容和绩点信息，但是这些内容并不会保存在或者发送到用户所属以外的任何地方。
+
+我们通过发送邮件的方式提醒成绩更新，但是需要用户自行配置发送邮件的邮箱，因此我们并不可能得知邮件的具体发送内容。我们也没有可能以任何方式获取用户的邮箱地址和 token。
+
+如果有更好的方法请教教我 QWQ 没做过相关的东西确实比较难办😪
+
+
+
+## 使用方法
+
+### 1 Fork 这个仓库
+
+![image-20220426235240714](README.assets/image-20220426235240714.png)
+
+请删除其中原有的 log.txt，否则代码会将其当做你的上一次结果进行比对（当然也不是不行）。
+
+
+
+### 2 生成一个 GitHub Token
+
+在 [Personal access tokens (github.com)](https://github.com/settings/tokens) 中 Generate 一个 new token，这是保存上次查询结果的需要：
+
+![image-20220426234832942](README.assets/image-20220426234832942.png)
+
+请注意这个 token 的有效期，在有效期结束后记得重新弄一个（当然也可以永久生效）！Select scopes 只需要勾选框住的部分即可QWQ
+
+
+
+### 3 配置一个发件邮箱
+
+SMTP 是发送电子邮件的协议，代码需要发送信箱的授权密码从而进行发件。
+
+请找一个邮箱，然后配置其自动发送功能。以 136 邮箱为例：
+
+<img src="README.assets/14C4D085D0E7DDF334C2821048D519A5.jpg" alt="img" style="zoom: 33%;" />
+
+<img src="README.assets/0418F40E7A323F8421905C67570FB6AE.jpg" alt="img" style="zoom:33%;" />
+
+开启 IMAP/SMTP 或者 POP3/SMTP 服务，记录下这个授权密码。
+
+然后，找到使用的邮箱的 SMTP 服务器地址（百度也可以）：
+
+<img src="README.assets/image-20220427121657608.png" alt="image-20220427121657608" style="zoom:50%;" />
+
+
+
+QQ 邮箱的话也是类似的操作，在 设置 - 账户 中开启后记住授权密码，并找到 SMTP 服务器信息即可：
+
+<img src="README.assets/image-20220427120230945.png" alt="image-20220427120230945" style="zoom: 67%;" />
+
+
+
+### 4 设置 Actions Secrets
+
+在你 fork 出的仓库的 Setting - Secrets - Actions 中新建 secret：
+
+![image-20220426233952791](README.assets/image-20220426233952791.png)
+
+总共需要如下 7 个 Secret（名字需要完全相同），分别表示：
+
+* `ACCOUNT` 和 `PASSWORD`：你的学号和统一身份认证密码
+* `TOKEN`：第 2 步配置的 GitHub Token
+* `MAILFROM` , `KEY ` 和 `HOST`：第 3 步配置的发件邮箱地址、授权密码和对应邮箱的 SMTP 服务器
+* `MAILTO`：收件邮箱地址，可以和之前那个一样
+
+<img src="README.assets/image-20220427121718613.png" alt="image-20220427121718613" style="zoom:80%;" />
+
+
+
+### 5 Enable Actions
+
+点到这里来！首先这里会让你 Enable 这个 Repo 的 Workflow；然后可能还需要点到左边 Workflows 下面的 ScoreQuery，点进去以后再 Enable 一遍）
+
+<img src="README.assets/16E0B535486AA410CBBF3AE0AD62BB66.png" alt="img" style="zoom: 50%;" />
+
+
+
+### 6 好啦！
+
+点击你自己仓库的 star（取消然后重新点也可以）就会触发一次操作。每 10 分钟也会自动进行一次查询。你可以在 Actions 中查看是否成功：
+
+![image-20220426235645511](README.assets/image-20220426235645511.png)
+
+
+
+### References
+
+除了文首参考的 [ZJU-Clock-In 自动打卡脚本](https://github.com/furry-potato-maker/ZJU-Clock-In) 以外，本工作的代码还部分参考自以下资料：
+
+* https://stackoverflow.com/questions/51657000/how-to-convert-an-html-table-into-a-python-dictionary
+* https://github.community/t/how-to-make-a-workflow-file-to-save-the-output-as-a-file-to-my-github-repo/18352/2
+* https://stackoverflow.com/questions/27522626/hash-function-in-python-3-3-returns-different-results-between-sessions
---
+++ scoreQuery.py
@@ -0,0 +1,212 @@
+import requests
+import json
+import re
+import datetime
+import time
+import sys
+import pandas
+import lxml
+import smtplib
+import threading
+from email.mime.text import MIMEText
+from email.header import Header
+
+def dft2(sess, headers):
+    try:
+        sess.get('http://jwbinfosys.zju.edu.cn/default2.aspx', headers=headers)
+    except:
+        pass
+	
+class ScoreQuery(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.login_url = "https://zjuam.zju.edu.cn/cas/login?service=http://jwbinfosys.zju.edu.cn"
+        self.base_url = "http://jwbinfosys.zju.edu.cn/xscj.aspx?xh=" + username
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+            'Connection': 'close'
+        }
+        self.sess = requests.Session()
+        print(username)
+        print(password)
+    
+    def login(self):
+        """Login to ZJU platform"""
+        res = self.sess.get(self.login_url, headers=self.headers)
+        execution = re.search(
+            'name="execution" value="(.*?)"', res.text).group(1)
+        res = self.sess.get(
+            url='https://zjuam.zju.edu.cn/cas/v2/getPubKey', headers=self.headers).json()
+        n, e = res['modulus'], res['exponent']
+        encrypt_password = self._rsa_encrypt(self.password, e, n)
+
+        data = {
+            'username': self.username,
+            'password': encrypt_password,
+            'execution': execution,
+            '_eventId': 'submit'
+        }
+        res = self.sess.post(url=self.login_url, data=data, headers=self.headers)
+
+        # check if login successfully
+        try:
+            dec = res.content.decode()
+            if '统一身份认证' in dec:
+                raise LoginError('登录失败，请核实账号密码重新登录')
+        except Exception as err:
+            pass
+        
+        return self.sess
+        
+    def _rsa_encrypt(self, password_str, e_str, M_str):
+        password_bytes = bytes(password_str, 'ascii')
+        password_int = int.from_bytes(password_bytes, 'big')
+        e_int = int(e_str, 16)
+        M_int = int(M_str, 16)
+        result_int = pow(password_int, e_int, M_int)
+        return hex(result_int)[2:].rjust(128, '0')
+        
+    def getInfo(self):
+        trialCount = 0
+        while trialCount < 10:
+            thread = threading.Thread(target = dft2, args = (self.sess, self.headers))
+            thread.start()
+            time.sleep(1)
+            
+            res = self.sess.get(self.base_url, headers = self.headers)
+            html = res.content.decode('gb2312')
+
+            matchResult = re.search('name="__VIEWSTATE" value="(.*?)"', res.text)
+            if (str(matchResult) != 'None'):
+                break
+            trialCount += 1
+            print("[log] 尝试读取数据失败，尝试第 " + str(trialCount) + " 次")
+        
+        if str(matchResult) == 'None':
+            exit('[Error] 读取数据失败。')
+        self.viewstate = matchResult.group(1)
+        
+        data = {
+            '__VIEWSTATE': self.viewstate,
+            'ddlXN': '',
+            'ddlXQ': '',
+            'txtQSCJ': '',
+            'txtZZCJ': '',
+            'Button2': '%D4%DA%D0%A3%D1%A7%CF%B0%B3%C9%BC%A8%B2%E9%D1%AF'
+        }
+        res = self.sess.post(url=self.base_url, data=data, headers=self.headers)
+        html = res.text
+
+        return html
+        
+class LoginError(Exception):
+    """Login Exception"""
+    pass
+    
+def myHash(text:str):
+   hash = 0
+   for ch in text:
+      hash = (hash * 281 ^ ord(ch) * 997) & 0xFFFFFFFF
+   return hash
+
+def sendMsg(newRecords, mailto, mailfrom, key):
+    host = "smtp.163.com"
+    port = 25
+    sender = mailfrom
+    receivers = [mailto]
+    
+    msg = MIMEText(newRecords, 'plain', 'utf-8')
+    msg['From'] = Header("auto_score_query@163.com")
+    msg['To'] = Header(mailto)
+    
+    subject = '出分啦！'
+    msg['Subject'] = Header(subject, 'utf-8')
+    
+    try:
+        smtpObj = smtplib.SMTP()
+        smtpObj.connect(host, port)
+        smtpObj.login(sender, key)
+        smtpObj.sendmail(sender, receivers, msg.as_string())
+        print("[status] 邮件已发送！")
+    except Exception as err:
+        print("[error] 邮件发送失败: " + str(err))
+    
+
+def main(username, password, mailto, mailfrom, key):
+    print("[status] 初始化...")
+    scoreQuery = ScoreQuery(username, password)
+    
+    print("[status] 登录...")
+    try:
+        scoreQuery.login()
+    except Exception as err:
+        print(str(err))
+        raise Exception
+        
+    print("[status] 获取数据...")
+    firstTry = 1
+    try:
+        res = scoreQuery.getInfo()
+    except Exception as err:
+        print(str(err))
+        raise Exception
+    
+    print("[status] 处理数据...")
+    data = pandas.read_html(res)
+    newData = {k: str(v).split()[8:] for k, v in data[2].groupby(0)}
+    del newData['选课课号']
+    newRecords = ''
+    try:
+        print("> 读取成绩记录列表...")
+        f = open("log.txt", "r")
+        
+        s = f.read()
+        records = set(s.split())
+        f.close()
+            
+        print("> 对比记录更新...")
+        for k, v in newData.items():
+            if str(myHash(k)) not in records:
+                newRecord = "课程名: " + v[0] + " 成绩: " + v[1] + " 学分: " + v[2] + " 绩点: " + v[3]
+                newRecords += '\n' + newRecord
+                print("    [!] 发现新记录: " + newRecord)
+        print("> 对比结束")
+    except Exception as err:
+        # first time
+        print(str(err))
+        print("[!] 此前暂无记录，正在生成首次记录...")
+        
+    tryCount = 5
+    while len(newRecords) != 0 and tryCount > 0:
+        try:
+            print("> 尝试发送邮件...")
+            sendMsg(newRecords, mailto, mailfrom, key)
+            break
+        except Exception as err:
+            print("[Error] 发送失败: " + str(err))
+            tryCount -= 1
+    
+    if tryCount == 0:
+        print("[Error] 发送失败。")
+        exit(1)
+        
+    print("[status] Writing data...")
+    f = open("log.txt", "w")
+    for k, v in newData.items():
+        #print(k, ":", v)
+        f.write(str(myHash(k)) + ' ')
+        #print(myHash(k))
+    f.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    mailto = sys.argv[3]
+    mailfrom = sys.argv[4]
+    key = sys.argv[5]
+    try:
+        main(username, password, mailto, mailfrom, key)
+    except Exception as err:
+        print(str(err))
+        exit(1)
